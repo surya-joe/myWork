2521af24add80772880dcd0def38467f73154355
surya-joe <suryajjoe@gmail.com>
2021-12-25 19:39:48 +0530

    santa

---
+++ templates/usingJS/3D-Wall-text/script.js
@@ -0,0 +1,17 @@
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
